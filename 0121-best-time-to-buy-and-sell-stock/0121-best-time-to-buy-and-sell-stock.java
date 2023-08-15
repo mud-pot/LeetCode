2d6a53471a99158c1793bb1acd8ad03eb4f06d98
mud-pot <88235348+mud-pot@users.noreply.github.com>
2023-08-16 01:36:53 +0530

    Time: 2 ms (88%), Space: 57 MB (96.48%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -18,19 +18,12 @@
 // }
 class Solution {
     public int maxProfit(int[] prices) {
-        int lsf = Integer.MAX_VALUE;
-        int op = 0;
-        int pist = 0;
-        
+        int leastBuy = Integer.MAX_VALUE;
+        int maxProfit = 0; 
         for(int i = 0; i < prices.length; i++){
-            if(prices[i] < lsf){
-                lsf = prices[i];
-            }
-            pist = prices[i] - lsf;
-            if(op < pist){
-                op = pist;
-            }
+            leastBuy = Math.min(prices[i], leastBuy);
+            maxProfit = Math.max((prices[i] - leastBuy), maxProfit);
         }
-        return op;
+        return maxProfit;
     }
 }
